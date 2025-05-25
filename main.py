646b25616b87af9eda4c4e8919d7663b278e4e92
th3br4vetrave1er <tazdingodan@gmail.com>
2025-05-25 13:26:47 +0300

    final brushes

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
 text = input("Tell me, traveler, how was your day?: ")
-docname = datetime.now().strftime("%Y-%m-%d")
+filename = datetime.now().strftime("%Y-%m-%d")
 
-with open(docname, "w") as doc:
-    doc.write(f"{docname}: {text}\n")
+with open(filename, "w") as file:
+    file.write(f"{filename}: {text}\n")
 
 print("Huh! Well that was something! Thank you for your story, see you tomorrow!")
